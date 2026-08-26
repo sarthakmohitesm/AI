4119604fde89e6767bf08b6ed66e4a79adb7380c
sarthak <sarthakmohitesm@gmail.com>
2026-08-26 21:25:33 +0530

    pratice for loop

---
+++ loops.js
@@ -0,0 +1,15 @@
+// const num=5;
+// for (i=1;i<=10;i++){
+//     var table = num*i;
+//     console.log(table); 
+// }
+
+
+// const num=5;
+
+// let num = Number(prompt("Enter the number"));
+
+let num=parseInt(prompt("Enter the number"));
+for (i=1;i<=10;i++){
+    console.log(`${num} x ${i} = ${num*i}`)
+}
